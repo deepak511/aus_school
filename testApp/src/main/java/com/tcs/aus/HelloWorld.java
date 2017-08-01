acedd261edb549d32937d389725079d7927fc489
Deepak <dswain@wiley.com>
2017-08-01 19:40:55 +0530

    AS-1|Test App project

---
+++ testApp/src/main/java/com/tcs/aus/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.tcs.aus;
+
+/**
+ * Hello world!
+ *
+ */
+public class HelloWorld 
+{
+	private String name;
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void printHello() {
+		System.out.println("Hello ! " + name);
+	}
+}
